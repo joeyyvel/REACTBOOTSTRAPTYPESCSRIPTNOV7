53215994b9dc2cf71ec096f8fa0e9f3294bc5c95
joeyboi <66668752+joeyyvel@users.noreply.github.com>
2024-11-07 20:35:49 +0800

    second commit

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,5 @@
 import "./App.css";
+import ChildWithHeader from "./ChildWithHeader";
 
 import { Header } from "./Header";
 
@@ -7,6 +8,9 @@ function App() {
     <>
       <h1>My App!</h1>
       <Header title="hello main menu" />
+      <ChildWithHeader title="title on main app">
+        Hello Children
+      </ChildWithHeader>
     </>
   );
 }
--- src/Header.tsx
+++ src/Header.tsx
@@ -8,7 +8,7 @@ export const Header = ({
   return (
     <>
       <div>
-        <h1>{title}</h1>
+        <h1 style={{ backgroundColor: "orange" }}>{title}</h1>
         <div className="card" style={{ width: "18 rem" }}>
           <img
             src="https://images.unsplash.com/photo-1528965051322-6d335c69eb86?q=80&w=1633&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D"
@@ -47,6 +47,7 @@ export const Header = ({
           </p>
         </div>
       </div>
+      <p style={{ backgroundColor: "orange" }}>Hello end!</p>
     </>
   );
 };
